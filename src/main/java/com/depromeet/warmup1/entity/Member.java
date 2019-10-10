954e062b7cb2fc712b17e8624617e2f6e8fa24fe
MaKyuSeok <mks502@naver.com>
2019-10-10 20:28:47 +0900

    feature/login start
    oauth (kakao) login을 위한
    RestTemplate 셋팅
    및
    Member entity 셋팅

---
+++ src/main/java/com/depromeet/warmup1/entity/Member.java
@@ -0,0 +1,28 @@
+package com.depromeet.warmup1.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Member {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column
+    private Long mid;
+	private String kakaoId;
+	private String name;
+	private String profileImgUrl;
+	private String firebaseToken;
+}
