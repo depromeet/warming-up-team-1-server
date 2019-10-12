78321de9d3780bc5ac713376e91ed7e5e52773c2
yeeeeerin <ebbunbul@gmail.com>
2019-10-12 15:12:49 +0900

    Add repository, service, swagger

---
+++ src/main/java/com/depromeet/warmup1/config/SwaggerConfig.java
@@ -0,0 +1,10 @@
+package com.depromeet.warmup1.config;
+
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@EnableSwagger2
+@Configuration
+public class SwaggerConfig {
+
+}
---
+++ src/main/java/com/depromeet/warmup1/service/TransactionService.java
@@ -0,0 +1,7 @@
+package com.depromeet.warmup1.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TransactionService {
+}
