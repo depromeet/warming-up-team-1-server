f89fa014980b03310386779be4d3db958c3f4686
yeeeeerin <ebbunbul@gmail.com>
2019-10-24 19:51:43 +0900

    fix member 관련 설정들

--- src/main/java/com/depromeet/warmup1/repository/MemberRepository.java
+++ src/main/java/com/depromeet/warmup1/repository/MemberRepository.java
@@ -7,11 +7,11 @@ import java.util.List;
 import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
-    public Member findOneByMid(Long mid);
+    Member findOneByMid(Long mid);
 
-    public Member findOneByName(String name);
+    Member findOneByName(String name);
 
-    public List<Member> findAllByConnectKey(String connectKey);
+    List<Member> findAllByConnectKey(String connectKey);
 
-    public Optional<Member> findOneByKakaoId(String id);
+    Optional<Member> findOneByKakaoId(String id);
 }
--- src/main/java/com/depromeet/warmup1/service/impl/MemberServiceImpl.java
+++ src/main/java/com/depromeet/warmup1/service/impl/MemberServiceImpl.java
@@ -6,100 +6,101 @@ import com.depromeet.warmup1.dto.LoginDto;
 import com.depromeet.warmup1.entity.Member;
 import com.depromeet.warmup1.exception.ApiFailedException;
 import com.depromeet.warmup1.exception.NotFoundException;
-import com.depromeet.warmup1.repostiroy.MemberRepository;
+import com.depromeet.warmup1.repository.MemberRepository;
 import com.depromeet.warmup1.service.MemberService;
-import java.time.LocalDateTime;
-import java.util.List;
+import com.depromeet.warmup1.util.UtilEncoder;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import com.depromeet.warmup1.util.UtilEncoder;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class MemberServiceImpl implements MemberService {
-	private final JwtFactory jwtFactory;
-	@Autowired
-	private KakaoAdapter kakaoAdapter;
-	@Autowired
-	private MemberRepository memberRepository;
-	@Autowired
-	private UtilEncoder utilEncoder;
+    private final JwtFactory jwtFactory;
+    @Autowired
+    private KakaoAdapter kakaoAdapter;
+    @Autowired
+    private MemberRepository memberRepository;
+    @Autowired
+    private UtilEncoder utilEncoder;
 
-	@Override
-	@Transactional(readOnly = true)
-	public Member getMemberByMid(Long mid) {
-		return memberRepository.findById(mid)
-				.orElseThrow(() -> new ApiFailedException("Member is not found.", HttpStatus.NOT_FOUND));
-	}
+    @Override
+    @Transactional(readOnly = true)
+    public Member getMemberByMid(Long mid) {
+        return memberRepository.findById(mid)
+                .orElseThrow(() -> new ApiFailedException("Member is not found.", HttpStatus.NOT_FOUND));
+    }
 
-	/**
-	 * login
-	 *
-	 * @param accessToken
-	 * @return Member
-	 */
-	@Override
-	@Transactional
-	public LoginDto getOrCreateMember(String kakaoToken) {
-		final KakaoUserDto kakaoUserDto = kakaoAdapter.getUserInfo(kakaoToken);
-		if (kakaoUserDto == null) {
-			throw new ApiFailedException("Failed to get user info from kakao api", HttpStatus.SERVICE_UNAVAILABLE);
-		}
-		final String kakaoId = kakaoUserDto.getId().toString();
-		final String profileImgUrl = kakaoUserDto.getProfileImage();
-		final String name = kakaoUserDto.getUserName();
-		Member member = memberRepository.findOneByKakaoId(kakaoId).orElseGet(() -> {
-			final Member m = new Member();
-			m.setKakaoId(kakaoId);
-			m.setProfileImgUrl(profileImgUrl);
-			m.setName(name);
-			return memberRepository.save(m);
-		});
+    /**
+     * login
+     *
+     * @param accessToken
+     * @return Member
+     */
+    @Override
+    @Transactional
+    public LoginDto getOrCreateMember(String kakaoToken) {
+        final KakaoUserDto kakaoUserDto = kakaoAdapter.getUserInfo(kakaoToken);
+        if (kakaoUserDto == null) {
+            throw new ApiFailedException("Failed to get user info from kakao api", HttpStatus.SERVICE_UNAVAILABLE);
+        }
+        final String kakaoId = kakaoUserDto.getId().toString();
+        final String profileImgUrl = kakaoUserDto.getProfileImage();
+        final String name = kakaoUserDto.getUserName();
+        Member member = memberRepository.findOneByKakaoId(kakaoId).orElseGet(() -> {
+            final Member m = new Member();
+            m.setKakaoId(kakaoId);
+            m.setProfileImgUrl(profileImgUrl);
+            m.setName(name);
+            return memberRepository.save(m);
+        });
 
-		return LoginDto.from(jwtFactory.generateToken(member), jwtFactory.generateRefreshToken(member));
+        return LoginDto.from(jwtFactory.generateToken(member), jwtFactory.generateRefreshToken(member));
 
-	}
+    }
 
-	@Override
-	public String getJwtToken(String refreshToken) {
-		Long id = jwtFactory.getMemberId(refreshToken).get();
-		Member member = memberRepository.findById(id).orElseThrow(NotFoundException::new);
+    @Override
+    public String getJwtToken(String refreshToken) {
+        Long id = jwtFactory.getMemberId(refreshToken).get();
+        Member member = memberRepository.findById(id).orElseThrow(NotFoundException::new);
 
-		return jwtFactory.generateToken(member);
+        return jwtFactory.generateToken(member);
 
-	}
+    }
 
-	@Override
-	@Transactional
-	public String createConnectKey(Long mid) {
-		Member member = memberRepository.findOneByMid(mid);
-		if (member == null)
-			return null;
-		String target = member.getMid() + ":" + LocalDateTime.now().toString();
-		member.setConnectKey(utilEncoder.encoding(target));
-		memberRepository.save(member);
-		return member.getConnectKey();
-	}
+    @Override
+    @Transactional
+    public String createConnectKey(Long mid) {
+        Member member = memberRepository.findOneByMid(mid);
+        if (member == null)
+            return null;
+        String target = member.getMid() + ":" + LocalDateTime.now().toString();
+        member.setConnectKey(utilEncoder.encoding(target));
+        memberRepository.save(member);
+        return member.getConnectKey();
+    }
 
-	@Override
-	@Transactional(readOnly = true)
-	public List<Member> getCouple(String connectKey) {
-		return memberRepository.findAllByConnectKey(connectKey);
-	}
+    @Override
+    @Transactional(readOnly = true)
+    public List<Member> getCouple(String connectKey) {
+        return memberRepository.findAllByConnectKey(connectKey);
+    }
 
-	@Override
-	@Transactional
-	public Member connectMember(Long mid, String connectKey) {
-		List<Member> members = getCouple(connectKey);
-		Member member = memberRepository.findOneByMid(mid);
-		if (member == null)
-			return null;
-		if (members.size() > 2 | members.size() == 0)
-			return null;
-		member.setConnectKey(connectKey);
-		return memberRepository.save(member);
-	}
+    @Override
+    @Transactional
+    public Member connectMember(Long mid, String connectKey) {
+        List<Member> members = getCouple(connectKey);
+        Member member = memberRepository.findOneByMid(mid);
+        if (member == null)
+            return null;
+        if (members.size() > 2 | members.size() == 0)
+            return null;
+        member.setConnectKey(connectKey);
+        return memberRepository.save(member);
+    }
 }
